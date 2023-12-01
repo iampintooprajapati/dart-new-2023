e7f87fab9a8f8e3140f9d8bc9651ce48ebc61470
iampintooprajapti <iampintooprajapati@gmail.com>
2023-12-01 18:45:45 +0530

    counter app with cubit bloc

---
+++ counter_app/lib/inc_dec_page.dart
@@ -0,0 +1,55 @@
+import 'package:counter_app/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncrementDecrementPage extends StatelessWidget {
+  const IncrementDecrementPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, int>(
+                bloc: counterCubit,
+                builder: (context, counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counterCubit.decrement();
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
