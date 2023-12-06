52f450d9d180e38fa974136bee454fd28b9766c7
iampintooprajapti <iampintooprajapati@gmail.com>
2023-12-06 13:02:58 +0530

    push notification foreground

--- push_notification_app/lib/main.dart
+++ push_notification_app/lib/main.dart
@@ -1,14 +1,22 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:push_notification_app/firebase_options.dart';
 import 'package:push_notification_app/home_page.dart';
+import 'package:push_notification_app/message.dart';
 import 'package:push_notification_app/push_notifications.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 // function to listen to background changes
 Future _firebaseBackgroundMessage(RemoteMessage message) async {
   if (message.notification != null) {
-    print("Some notification Received ");
+    if (kDebugMode) {
+      print("Some notification Received ");
+    }
   }
 }
 
@@ -17,9 +25,36 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // on background notification tapped
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    if (message.notification != null) {
+      if (kDebugMode) {
+        print("background notification tapped");
+      }
+      navigatorKey.currentState!.pushNamed("/message", arguments: message);
+    }
+  });
   PushNotifications.init();
+  PushNotifications.localNotiInit();
+  
   // Listen to background notifications
   FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessage);
+
+  // to handle foreground notification
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    String payloadData = jsonEncode(message.data);
+    if (kDebugMode) {
+      print("Got a message in foreground");
+    }
+    if (message.notification != null) {
+      PushNotifications.showSimpleNotification(
+          title: message.notification!.title!,
+          body: message.notification!.body!,
+          payload: payloadData);
+    }
+  });
+
   runApp(const MyApp());
 }
 
@@ -29,12 +64,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/message': (context) => const Message()
+      },
+      // home: const HomePage(),
     );
   }
 }
